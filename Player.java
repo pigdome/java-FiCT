537eee1c341444042953e6728e1824db68db8c12
apirak <apirak@punsarn.asia>
2019-03-08 22:36:55 +0700

    first commit

---
+++ Player.java
@@ -0,0 +1,134 @@
+
+public class Player {
+
+	public enum PlayerType {Healer, Tank, Samurai, BlackMage, Phoenix, Cherry};
+	
+	private PlayerType type; 	//Type of this player. Can be one of either Healer, Tank, Samurai, BlackMage, or Phoenix
+	private double maxHP;		//Max HP of this player
+	private double currentHP;	//Current HP of this player 
+	private double atk;			//Attack power of this player
+	
+	
+	/**
+	 * Constructor of class Player, which initializes this player's type, maxHP, atk, numSpecialTurns, 
+	 * as specified in the given table. It also reset the internal turn count of this player. 
+	 * @param _type
+	 */
+	public Player(PlayerType _type)
+	{	
+		//INSERT YOUR CODE HERE
+	}
+	
+	/**
+	 * Returns the current HP of this player
+	 * @return
+	 */
+	public double getCurrentHP()
+	{
+		//INSERT YOUR CODE HERE
+		return 0;
+	}
+	
+	/**
+	 * Returns type of this player
+	 * @return
+	 */
+	public Player.PlayerType getType()
+	{
+		//INSERT YOUR CODE HERE
+		return null;
+	}
+	
+	/**
+	 * Returns max HP of this player. 
+	 * @return
+	 */
+	public double getMaxHP()
+	{
+		//INSERT YOUR CODE HERE
+		
+		return 0;
+	}
+	
+	/**
+	 * Returns whether this player is sleeping.
+	 * @return
+	 */
+	public boolean isSleeping()
+	{
+		//INSERT YOUR CODE HERE
+		
+		return false;
+	}
+	
+	/**
+	 * Returns whether this player is being cursed.
+	 * @return
+	 */
+	public boolean isCursed()
+	{
+		//INSERT YOUR CODE HERE
+		
+		return false;
+	}
+	
+	/**
+	 * Returns whether this player is alive (i.e. current HP > 0).
+	 * @return
+	 */
+	public boolean isAlive()
+	{
+		//INSERT YOUR CODE HERE
+		
+		return true;
+	}
+	
+	/**
+	 * Returns whether this player is taunting the other team.
+	 * @return
+	 */
+	public boolean isTaunting()
+	{
+		//INSERT YOUR CODE HERE
+		return false;
+	}
+	
+	
+	public void attack(Player target)
+	{	
+		//INSERT YOUR CODE HERE
+	}
+	
+	public void useSpecialAbility(Player[][] myTeam, Player[][] theirTeam)
+	{	
+		//INSERT YOUR CODE HERE
+	}
+	
+	
+	/**
+	 * This method is called by Arena when it is this player's turn to take an action. 
+	 * By default, the player simply just "attack(target)". However, once this player has 
+	 * fought for "numSpecialTurns" rounds, this player must perform "useSpecialAbility(myTeam, theirTeam)"
+	 * where each player type performs his own special move. 
+	 * @param arena
+	 */
+	public void takeAction(Arena arena)
+	{	
+		//INSERT YOUR CODE HERE
+	}
+	
+	/**
+	 * This method overrides the default Object's toString() and is already implemented for you. 
+	 */
+	@Override
+	public String toString()
+	{
+		return "["+this.type.toString()+" HP:"+this.currentHP+"/"+this.maxHP+" ATK:"+this.atk+"]["
+				+((this.isCursed())?"C":"")
+				+((this.isTaunting())?"T":"")
+				+((this.isSleeping())?"S":"")
+				+"]";
+	}
+	
+	
+}
